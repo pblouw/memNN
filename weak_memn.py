Try to fix gradient.

weak_memn.py
@@ -82,13 +82,40 @@ class WeakMemoryNetwork(object):
             # Error gradient on 4-way softmax output
             r_grad = prediction - target      
 
+            def softmax_jacobian(o):
+                a = modules.Module.softmax(o)
+                return np.diag(a) - np.outer(a, a)
+
             # Update R based on this gradient
             self._response.update_parameters(r_grad, q_embed+o_embed)  
 
             # Error gradient wrt input to R module
-            r_input_grad = np.dot(self._response.choices.T, r_grad)    
+
+
+            # v^T W^T M^T = q_embed.T
+            a = np.dot(q_embed.T, self._output.memory.T)
+            J1 = softmax_jacobian(a)
+            b = np.dot(self._output.memory, self._response.choices.T)
+            c = np.dot(np.dot(b.T, J1), self._output.memory)
+            d = np.dot(self._response.choices, q_embed + o_embed)
+            J2 = softmax_jacobian(d)
+            e = np.sum(r_grad[:, None] * np.dot(J2, c), axis=0)
             self._input.update_parameters(
-                    r_input_grad, self.vectorizer(query.text).flatten())
+                e, self.vectorizer(query.text).flatten())
+
+
+            self._response.build_choices(query)
+            q_embed = self._input.encode_question(query)
+            o_embed = self._output.encode_output_features(q_embed)
+
+            prediction2 = self._response.predict(q_embed+o_embed)
+            r_grad2 = prediction2 - target      
+            print target, prediction, prediction2, np.linalg.norm(r_grad2) - np.linalg.norm(r_grad)
+            assert np.linalg.norm(r_grad2) - np.linalg.norm(r_grad) <= 0
+
+            #r_input_grad = np.dot(self._response.choices.T, r_grad)    
+            #self._input.update_parameters(
+                    #r_input_grad, self.vectorizer(query.text).flatten())
 
             # TODO: Figure out update to Input module params based on gradient
             # Right now the R module parameters are the only thing being trained