b65f936f7a29ae1c86f9947d449724dd085c5a23
Jan Gosmann <jan@hyper-world.de>
2015-12-15 14:30:00 +0100

    Add psyrun task for batch processing.

---
+++ Weak Memory Network/psytasks/task_batch.py
@@ -0,0 +1,76 @@
+import cPickle as pickle
+import sys
+sys.path.insert(0, '..')
+
+from psyrun import Param, map_pspace_parallel
+
+from mctest_load import load_stories
+from weak_memn import WeakMemoryNetwork
+
+
+pspace = Param(
+    n_epochs=5,
+    timetags=[False, True, False, False, False, True],
+    word2vec=[False, False, True, False, False, True],
+    roles=[False, False, False, True, False, True],
+    coref=[False, False, False, False, True, True]) * Param(trial=range(1))
+mapper = map_pspace_parallel
+
+def clean(stories):
+    return [s for s in stories if len(s.queries) > 1]
+
+
+def compute_accuracy(stories, model):
+    accuracy = 0
+    for story in stories:
+        score = model.predict_answer(story)
+        accuracy += score
+    return float(accuracy) / float(len(stories))
+
+
+def load_data(coref):
+    if coref:
+        with open('MCTest/mc160.train.coref', 'rb') as f:
+            train_stories = clean(pickle.load(f))
+        with open('MCTest/mc160.dev.coref', 'rb') as f:
+            dev_stories = clean(pickle.load(f))
+        with open('MCTest/mc160.test.coref', 'rb') as f:
+            test_stories = clean(pickle.load(f))
+    else:
+        train_stories = clean(load_stories(
+            'MCTest/mc160.train.tsv', 'MCTest/mc160.train.ans'))
+        dev_stories = clean(load_stories(
+            'MCTest/mc160.dev.tsv', 'MCTest/mc160.dev.ans'))
+        test_stories = clean(load_stories(
+            'MCTest/mc160.test.tsv', 'MCTest/mc160.test.ans'))
+    return train_stories, dev_stories, test_stories
+
+
+def execute(trial, n_epochs, timetags, word2vec, roles, coref):
+    train_stories, dev_stories, test_stories = load_data(coref)
+    all_stories = train_stories + test_stories + dev_stories
+
+    if timetags:
+        d = 556
+    else:
+        d = 300
+    model = WeakMemoryNetwork(
+        d, all_stories, timetags=timetags, word2vec=word2vec, roles=roles,
+        coref=coref)
+
+    pre_train_acc = compute_accuracy(train_stories, model)
+    pre_test_acc = compute_accuracy(test_stories, model)
+
+    for _ in range(n_epochs):
+        for story in train_stories + dev_stories:
+            model.train(story)
+
+    post_train_acc = compute_accuracy(train_stories, model)
+    post_test_acc = compute_accuracy(test_stories, model)
+
+    return {
+        'pre_train_acc': pre_train_acc,
+        'pre_test_acc': pre_test_acc,
+        'post_train_acc': post_train_acc,
+        'post_test_acc': post_test_acc,
+    }
